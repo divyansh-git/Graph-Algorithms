3c01ce179a02c842c167d793dd21a9540ecc3d47
divyansh rastogi <rastogi11204@gmail.com>
2021-05-30 12:39:18 +0530

    DFS added

---
+++ DFS.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<unordered_map>
+#include<list>
+#include<vector>
+using namespace std;
+class Graph
+{
+    public:
+    unordered_map<int,list<pair<int,int> > > g;
+    void addEdge(int a, int b, int w, int bidir=1)
+    {
+        g[a].push_back(make_pair(b,w));
+        if(bidir)
+        g[b].push_back(make_pair(a,w));
+    }
+    void DFS_helper(int source,vector<int> &visited)
+    {
+        visited[source]=1;
+        cout<<source<<" ";
+        for(auto node: g[source])
+        {
+            if(!visited[node.first])
+            {
+                visited[node.first]=1;
+                DFS_helper(node.first,visited);
+            }
+        }
+    }
+    void DFS(int n)
+    {
+        vector<int> visited(n+1,0);
+        for(int i=1;i<=n;i++)
+        {
+            if(!visited[i])
+            DFS_helper(i,visited);
+        }
+    }
+
+};
+int main()
+{
+   #ifndef ONLINE_JUDGE
+   freopen("input.txt", "r", stdin);
+   freopen("output.txt", "w", stdout);
+   #endif
+    Graph g;
+    int n,e,a,w,b;
+    bool bidir;
+    cin>>n>>e;
+    for(int i=0;i<e;i++)
+    {
+        cin>>a>>b>>w>>bidir;
+        g.addEdge(a,b,w,bidir);
+    }
+    g.DFS(n);
+    return 0;
+}
